73d6e94ed8e91b298d7648c83fcea737bba54780
kaibalya jena <kaibalya18j@gmail.com>
2022-08-23 10:13:19 +0530

    testimonials

---
+++ script.js
@@ -0,0 +1,27 @@
+testimonials=[
+    {
+        name:"kaibalya jena",
+        age:19,
+        abilities:"sports and studies",
+        testimonial:"well how can a person write his own testimonial"
+    },
+    {
+        name:"twella vaidya",
+        age:20,
+        abilities:"surgery and medicine",
+        testimonial:"all i can say is she is perfect"
+    },
+    {
+        name:"sdflhij",
+        age:19,
+        abilities:"a pro web developer",
+        testimonial:"well how can a person write his own testimonial"
+    },
+    {
+        name:"at",
+        age:19,
+        abilities:"simplicity and naiveness",
+        testimonial:"well we all can figure that out"
+    }
+    
+]
